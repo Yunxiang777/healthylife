aae0d6cf52042378f712b848834ddeee540f5365
Yunxiang777 <yunxiangwang777@gmail.com>
2023-12-15 04:50:29 +0800

    修改登入頁面，表單判斷與登入判斷

--- controller/login_check.php
+++ controller/login_check.php
@@ -3,14 +3,17 @@ require __DIR__ . '/../model/login_check.php';
 $Login_check = new Login_check();
 
 //確認使用者觸發帳號密碼登入
-if ($_POST['email']) {
-    $result = $Login_check->login_confirm($_POST['email'], $_POST['password']);
-    //確認帳號密碼是否存在
-    if ($result) {
-        echo $result['id'];
-        exit();
-    } else {
-        echo false;
-        exit();
-    }
+$result = $Login_check->login_confirm($_POST['email'], $_POST['password']);
+if ($result) {
+    session_start();
+    $_SESSION['member_id'] = $result['id'];
+    $_SESSION['email'] = $result['email'];
+    $_SESSION['login_status'] = true;
+    header('Content-Type: application/json');
+    echo json_encode(['login_status' => true]);
+    exit();
+} else {
+    $_SESSION['login_status'] = false;
+    echo json_encode(['login_status' => false]);
+    exit();
 }
--- view/login.php
+++ view/login.php
@@ -6,12 +6,12 @@
 <div class="container">
     <form class="form_style">
         <div class="form-group">
-            <label for="email">郵件地址</label>
+            <label for="email">郵件地址 <span id="mail_check"></span></label>
             <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
             <small id="emailHelp" class="form-text text-muted">我們絕對不會洩漏您個人隱私</small>
         </div>
         <div class="form-group">
-            <label for="password">密碼</label>
+            <label for="password">密碼 <span id="password_check"></span></label>
             <input type="password" class="form-control" id="password">
         </div>
         <div class="form-group form-check">
@@ -28,6 +28,10 @@ $(document).ready(function() {
     $("#login").click(function() {
         var email = $("#email").val();
         var password = $("#password").val();
+
+        //檢查帳號密碼輸入格式
+        if (!validateLogin(email, password)) return;
+
         $.ajax({
             type: "POST",
             url: "../controller/login_check.php",
@@ -36,11 +40,8 @@ $(document).ready(function() {
                 password: password
             },
             success: function(response) {
-                if (response) {
-                    window.location.href = '../index.php';
-                } else {
-                    console.log("帳號密碼有誤");
-                }
+                //檢查帳號密碼是否正確
+                login_check(response.login_status);
             },
             error: function(error) {
                 console.error("Ajax request failed", error);
@@ -48,5 +49,48 @@ $(document).ready(function() {
         });
 
     });
+
 });
+
+//檢查帳號密碼輸入格式
+function validateLogin(email, password) {
+    var loginErrors = {};
+
+    // Check email
+    if (!email.trim()) {
+        loginErrors.email = "Email 不能為空";
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+        loginErrors.email = "Email 格式錯誤";
+    } else {
+        loginErrors.email = '';
+    }
+
+    // Check password
+    if (!password.trim()) {
+        loginErrors.password = "密碼不能為空";
+    } else {
+        loginErrors.password = '';
+    }
+
+    if (loginErrors.email || loginErrors.password) {
+        $("#mail_check").html(loginErrors.email).css("display", "inline");
+        $("#password_check").html(loginErrors.password).css("display", "inline");
+        return false; // Exit the function if there are errors
+    } else {
+        return true;
+    }
+
+}
+
+//檢查帳號密碼是否正確
+function login_check(login_status) {
+    if (login_status) {
+        $("#mail_check").html('').css("display", "none");
+        $("#password_check").html('').css("display", "none");
+        window.location.href = '../index.php';
+    } else {
+        $("#mail_check").html('帳號密碼錯誤').css("display", "inline");
+        $("#password_check").html('帳號密碼錯誤').css("display", "inline");
+    }
+}
 </script>
