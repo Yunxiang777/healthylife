266710f0b6db520a8e4821582509c30e1aadc5a1
Yunxiang777 <yunxiangwang777@gmail.com>
2023-12-17 02:01:01 +0800

    解決header網址轉向，緩衝輸出

--- view/diet_record.php
+++ view/diet_record.php
@@ -1,7 +1,7 @@
 <?php
 require __DIR__ . '/template/narbar.php';
-require __DIR__ . '/../model/get_food_record.php';
 $helper->visitor_redirect();
+require __DIR__ . '/../model/get_food_record.php';
 
 // 獲取食物記錄結果
 $foodRecordGetter = new Get_food_record();
@@ -15,8 +15,7 @@ $result = $foodRecordGetter->getFoodRecord($formattedDate, $_SESSION['member_id'
     <h3><?= $mealTime ?></h3>
     <h5>今日總熱量攝取 <?= $foodRecordGetter->today_calories($formattedDate, $_SESSION['member_id']) ?></h5>
     <div class="progress">
-        <div class="progress-bar bg-info" role="progressbar" style="width: 10%" aria-valuenow="50" aria-valuemin="0"
-            aria-valuemax="100">153</div>
+        <div class="progress-bar bg-info" role="progressbar" style="width: 10%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">153</div>
     </div>
     <div class="food_box">
         <input type="text" id="food" placeholder="食物" class="input_diet">
@@ -36,52 +35,52 @@ $result = $foodRecordGetter->getFoodRecord($formattedDate, $_SESSION['member_id'
 </div>
 
 <script>
-$(document).ready(function() {
-    //新增食物觸發
-    $("#add_food").on("click", function() {
-        // 獲取食物和卡路里的值
-        var foodName = $("#food").val();
-        var calories = $("#calories").val();
-        var today = formatDate(new Date());
+    $(document).ready(function() {
+        //新增食物觸發
+        $("#add_food").on("click", function() {
+            // 獲取食物和卡路里的值
+            var foodName = $("#food").val();
+            var calories = $("#calories").val();
+            var today = formatDate(new Date());
 
-        $.ajax({
-            type: "POST",
-            url: "../controller/food_record.php",
-            data: {
-                foodName: foodName,
-                calories: calories,
-                users_id: 1,
-                today: today,
-                mealTime: "<?= $mealTime ?>"
-            },
-            success: function(response) {
-                // 處理成功的回應
-                location.reload();
-            },
-            error: function(error) {
-                // 處理錯誤
-                console.error(error);
-            }
+            $.ajax({
+                type: "POST",
+                url: "../controller/food_record.php",
+                data: {
+                    foodName: foodName,
+                    calories: calories,
+                    users_id: 1,
+                    today: today,
+                    mealTime: "<?= $mealTime ?>"
+                },
+                success: function(response) {
+                    // 處理成功的回應
+                    location.reload();
+                },
+                error: function(error) {
+                    // 處理錯誤
+                    console.error(error);
+                }
+            });
         });
+
+        // 日期格式化函數
+        function formatDate(date) {
+            var dd = date.getDate();
+            var mm = date.getMonth() + 1;
+            var yyyy = date.getFullYear();
+
+            if (dd < 10) {
+                dd = '0' + dd;
+            }
+
+            if (mm < 10) {
+                mm = '0' + mm;
+            }
+
+            return yyyy + '-' + mm + '-' + dd;
+        }
     });
-
-    // 日期格式化函數
-    function formatDate(date) {
-        var dd = date.getDate();
-        var mm = date.getMonth() + 1;
-        var yyyy = date.getFullYear();
-
-        if (dd < 10) {
-            dd = '0' + dd;
-        }
-
-        if (mm < 10) {
-            mm = '0' + mm;
-        }
-
-        return yyyy + '-' + mm + '-' + dd;
-    }
-});
 </script>
 
-<?php $foodRecordGetter->closeConnection();?>
+<?php $foodRecordGetter->closeConnection(); ?>
--- view/template/narbar.php
+++ view/template/narbar.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../../helper.php';
 $helper = new Helper();
 //取得當前路由位置
 $filename = pathinfo(basename($_SERVER['REQUEST_URI']))['filename'];
+ob_start(); //緩衝輸出，確保header被呼叫之前，沒有任何輸出
 ?>
 
 <!DOCTYPE html>
@@ -19,8 +20,7 @@ $filename = pathinfo(basename($_SERVER['REQUEST_URI']))['filename'];
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="<?= $routing['base_url'] ?>">HealthyLife</a>
-        <button class=" navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class=" navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -49,8 +49,7 @@ $filename = pathinfo(basename($_SERVER['REQUEST_URI']))['filename'];
                 </li> -->
             </ul>
             <form class="form-inline my-2 my-lg-0">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="button"
-                    id="<?= $helper->login_status() ? 'logout_member' : 'login_member' ?>">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="button" id="<?= $helper->login_status() ? 'logout_member' : 'login_member' ?>">
                     <?= $helper->login_status() ? '登出' : '登入' ?>
                 </button>
             </form>
@@ -58,28 +57,28 @@ $filename = pathinfo(basename($_SERVER['REQUEST_URI']))['filename'];
         </div>
     </nav>
     <script>
-    $(document).ready(function() {
-        //登入會員
-        $("#login_member").click(function() {
-            window.location.href =
-                "<?= $routing['login'] ?>";
-        });
+        $(document).ready(function() {
+            //登入會員
+            $("#login_member").click(function() {
+                window.location.href =
+                    "<?= $routing['login'] ?>";
+            });
 
-        //登出
-        $("#logout_member").click(function() {
-            $.ajax({
-                url: "<?= $api['logout'] ?>",
-                type: "POST",
-                data: {
-                    'logout': true
-                },
-                success: function(data) {
-                    window.location.href = "<?= $routing['base_url'] ?>";
-                },
-                error: function(error) {
-                    console.error("Ajax request failed", error);
-                }
+            //登出
+            $("#logout_member").click(function() {
+                $.ajax({
+                    url: "<?= $api['logout'] ?>",
+                    type: "POST",
+                    data: {
+                        'logout': true
+                    },
+                    success: function(data) {
+                        window.location.href = "<?= $routing['base_url'] ?>";
+                    },
+                    error: function(error) {
+                        console.error("Ajax request failed", error);
+                    }
+                });
             });
         });
-    });
     </script>
